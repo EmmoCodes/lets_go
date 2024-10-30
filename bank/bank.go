0d01fc6eb740759cb3bba48e870ffa1277c264de
Marco Orlandi <Gianluca1996@gmx.de>
2024-10-30 16:14:49 +0100

    added get and set

--- bank/bank.go
+++ bank/bank.go
@@ -1,54 +1,142 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
 
-func main() {
-	var accountBalance = 1000.0
+const accountBalanceFile = "balance.txt"
 
-	println("Welcome to Gobber Bank")
-	println("What do you want to do?")
-	println("1. Check balance")
-	println("2. Deposit money")
-	println("3. Withdraw money")
-	println("4. Exit")
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile)
 
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
-
-	if choice == 1 {
-		fmt.Println("Your balance is:", accountBalance)
-	} else if choice == 2 {
-		fmt.Print("Your deposit: ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
-
-		if depositAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-			return
-		}
-
-		accountBalance += depositAmount
-		fmt.Println("Balance updated! New amount: ", accountBalance)
-	} else if choice == 3 {
-		fmt.Print("Withdrawal amount: ")
-		var withdrawalAmount float64
-		fmt.Scan(&withdrawalAmount)
-
-		if withdrawalAmount <= 0 {
-			fmt.Println("Invalid amount. Must be greater than 0.")
-			return
-		} else if withdrawalAmount > accountBalance {
-			fmt.Println("You dont have enough money!")
-			return
-		}
-
-		accountBalance -= withdrawalAmount
-		fmt.Println("Balance updated! New amount: ", accountBalance)
-	} else {
-		fmt.Println("Goodbye!")
+	if err != nil {
+		return 1000, errors.New("failed to find balance file")
 	}
 
-	fmt.Println("Your choice", choice)
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+
+	if err != nil {
+		return 1000, errors.New("failed to parse stoed balance value")
+	}
+
+	return balance, nil
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+}
+
+func main() {
+	var accountBalance, err = getBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		fmt.Println("----------")
+		// panic("Can't continue, sorry!")
+	}
+
+	// for i := 0; i < 2; i++ {
+	// we want an infinite loop
+	for {
+
+		println("Welcome to Gobber Bank")
+		println("What do you want to do?")
+		println("1. Check balance")
+		println("2. Deposit money")
+		println("3. Withdraw money")
+		println("4. Exit")
+
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is:", accountBalance)
+		case 2:
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				// return
+				continue
+			}
+
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+			writeBalanceToFile(accountBalance)
+		case 3:
+			fmt.Print("Withdrawal amount: ")
+			var withdrawalAmount float64
+			fmt.Scan(&withdrawalAmount)
+
+			if withdrawalAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+
+			if withdrawalAmount > accountBalance {
+				fmt.Println("You dont have enough money!")
+				continue
+			}
+
+			accountBalance -= withdrawalAmount
+			fmt.Println("Balance updated! New amount: ", accountBalance)
+			writeBalanceToFile(accountBalance)
+		default:
+			fmt.Println("Goodbye!")
+			fmt.Println("Thanks for choosing our bank.")
+			return
+			// break
+		}
+
+		// if choice == 1 {
+		// 	fmt.Println("Your balance is:", accountBalance)
+		// } else if choice == 2 {
+		// 	fmt.Print("Your deposit: ")
+		// 	var depositAmount float64
+		// 	fmt.Scan(&depositAmount)
+
+		// 	if depositAmount <= 0 {
+		// 		fmt.Println("Invalid amount. Must be greater than 0.")
+		// 		// return
+		// 		continue
+		// 	}
+
+		// 	accountBalance += depositAmount
+		// 	fmt.Println("Balance updated! New amount: ", accountBalance)
+		// } else if choice == 3 {
+		// 	fmt.Print("Withdrawal amount: ")
+		// 	var withdrawalAmount float64
+		// 	fmt.Scan(&withdrawalAmount)
+
+		// 	if withdrawalAmount <= 0 {
+		// 		fmt.Println("Invalid amount. Must be greater than 0.")
+		// 		return
+		// 	}
+
+		// 	if withdrawalAmount > accountBalance {
+		// 		fmt.Println("You dont have enough money!")
+		// 		return
+		// 	}
+
+		// 	accountBalance -= withdrawalAmount
+		// 	fmt.Println("Balance updated! New amount: ", accountBalance)
+		// } else {
+		// 	fmt.Println("Goodbye!")
+		// 	// return
+		// 	break
+		// }
+
+		fmt.Println("Your choice", choice)
+	}
 
 }
